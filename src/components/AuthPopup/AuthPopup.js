e0e64564967428eb944aa39afb12217759fa8e30
ILokalin <51790923+ILokalin@users.noreply.github.com>
2020-06-18 00:58:10 +0500

    refactor: correct format with linter

--- src/components/AuthPopup/AuthPopup.js
+++ src/components/AuthPopup/AuthPopup.js
@@ -1,36 +1,42 @@
 import { DomGen } from 'Service/DomGen';
 
 export function AuthPopup() {
-  const body = document.body;
+  const { body } = document;
   const popup = DomGen({
-    name: 'auth-popup', tag: 'div', children: [
-      {tag: 'div', className: "menu", children: [
-        {tag: 'input', className: "input", placeholder: "email"},
-        {tag: 'input', className: "input", placeholder: "password"},
-        {tag: 'button', className: "button", innerText: "Login", isAccess: 'login'},
-        {tag: 'button', className: "button", innerText: "Cancel", isAccess: 'cancel'}
-      ]}
-    ]
-  })
+    name: 'auth-popup',
+    tag: 'div',
+    children: [
+      {
+        tag: 'div',
+        className: 'menu',
+        children: [
+          { tag: 'input', className: 'input', placeholder: 'email' },
+          { tag: 'input', className: 'input', placeholder: 'password' },
+          { tag: 'button', className: 'button', innerText: 'Login', isAccess: 'login' },
+          { tag: 'button', className: 'button', innerText: 'Cancel', isAccess: 'cancel' },
+        ],
+      },
+    ],
+  });
 
-  body.append(popup.block)
+  body.append(popup.block);
 
   const removePopup = () => {
     popup.block.remove();
-  }
+  };
 
   return new Promise((resolve, reject) => {
     const tryLogin = () => {
       removePopup();
       resolve();
-    }
+    };
 
     const cancelAuth = () => {
       removePopup();
       reject();
-    }
+    };
 
     popup.login.addEventListener('click', tryLogin);
     popup.cancel.addEventListener('click', cancelAuth);
-  })
+  });
 }
