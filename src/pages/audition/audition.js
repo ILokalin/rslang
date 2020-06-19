dd975e6c3d918b5f5818d0d0383421f57bebb5c3
mc-gorky <mcgorky@gmail.com>
2020-06-20 01:00:56 +0300

    feat: add game rounds logic

--- src/pages/audition/audition.js
+++ src/pages/audition/audition.js
@@ -2,9 +2,14 @@ export default class AuditionGame {
   constructor () {
     this.user = localStorage.getItem('user') || 'Dick McDickennson';
     this.level = 0;
-    this.wordsTranslations = [];
-    this.puzzledWord = this.getPuzzledWord();
-    console.log(this.wordsTranslations);
+    this.round = 0;
+    this.currentWordsTranslations = [];
+    this.currentPuzzledWord = {};
+    this.nextWordsTranslations = [];
+    this.nextPuzzledWord = {};
+    this.startGame();
+    this.correctWords = [];
+    this.errorWords = [];
   }
 
   static getRandomNumber (max) {
@@ -18,32 +23,125 @@ export default class AuditionGame {
       .then((res) => res.json())
       .then((result) => {
         const randomNumber = AuditionGame.getRandomNumber(19);
-        const puzzledWord = {};
-        puzzledWord.word = result[randomNumber].word;
-        puzzledWord.wordTranslate = result[randomNumber].wordTranslate;
-        puzzledWord.audio = result[randomNumber].audio;
-        puzzledWord.image = result[randomNumber].image;
-        console.log(puzzledWord);
-        this.wordsTranslations.push(puzzledWord.wordTranslate);
-        return puzzledWord;
+        this.currentPuzzledWord.word = result[randomNumber].word;
+        this.currentPuzzledWord.wordTranslate = result[randomNumber].wordTranslate;
+        this.currentPuzzledWord.audio = new Audio(result[randomNumber].audio.replace('files', 'sound'));
+        this.currentPuzzledWord.image = result[randomNumber].image.replace('files', 'img');
       })
-      .then(() => this.getWordsTranslations())
+      .then(() => this.currentWordsTranslations.push(this.currentPuzzledWord.wordTranslate))
+      .then(() => setTimeout(() => this.currentPuzzledWord.audio.play(), 1000))
+      .then(() => this.getCurrentWordsTranslations())
   }
 
-  getWordsTranslations () {
+  getCurrentWordsTranslations () {
     const pageNumber = AuditionGame.getRandomNumber(29);
     const url = `https://afternoon-falls-25894.herokuapp.com/words?group=${this.level}&page=${pageNumber}`
     fetch(url)
       .then((res) => res.json())
       .then ((result) => {
-        while (this.wordsTranslations.length < 5) {
+        while (this.currentWordsTranslations.length < 5) {
           const randomNumber = AuditionGame.getRandomNumber(19);
           const word = result[randomNumber].wordTranslate;
-          if(!this.wordsTranslations.includes(word)) {
-            this.wordsTranslations.push(word);
+          if(!this.currentWordsTranslations.includes(word)) {
+            this.currentWordsTranslations.push(word);
           }
         }
-        this.wordsTranslations.sort(() => Math.random() - Math.random());
+        this.currentWordsTranslations.sort(() => Math.random() - Math.random());
       })
+      .then(() => this.writeRoundData())
+  }
+
+  getNextWordsTranslations () {
+    const pageNumber = AuditionGame.getRandomNumber(29);
+    const url = `https://afternoon-falls-25894.herokuapp.com/words?group=${this.level}&page=${pageNumber}`
+    fetch(url)
+      .then((res) => res.json())
+      .then ((result) => {
+        while (this.nextWordsTranslations.length < 5) {
+          const randomNumber = AuditionGame.getRandomNumber(19);
+          const word = result[randomNumber].wordTranslate;
+          if(!this.nextWordsTranslations.includes(word)) {
+            this.nextWordsTranslations.push(word);
+          }
+        }
+        this.nextWordsTranslations.sort(() => Math.random() - Math.random());
+      })
+  }
+
+  getRoundData () {
+    this.getPuzzledWord();
+
+    if (this.round < 9) {
+      // this.nextPuzzledWord = this.getPuzzledWord();
+      // // this.nextWordsTranslations.push(this.nextPuzzledWord.wordTranslate);
+      // this.getNextWordsTranslations();
+      // this.writeNextRoundData();
+    }
+  }
+
+  writeRoundData() {
+    const gameWrapper = document.createElement('div');
+    gameWrapper.classList.add('game-wrapper');
+    document.body.append(gameWrapper);
+
+    const wordImageEl = document.createElement('div');
+    wordImageEl.classList.add('word-image-element');
+    wordImageEl.style.backgroundImage = `url(${this.currentPuzzledWord.image})`;
+    gameWrapper.append(wordImageEl);
+
+    const audioEl = document.createElement('div');
+    audioEl.classList.add('audio-element');
+    audioEl.addEventListener('click', () => {
+      this.currentPuzzledWord.audio.play();
+    })
+    gameWrapper.append(audioEl);
+
+    const puzzledWordEl = document.createElement('div');
+    puzzledWordEl.classList.add('puzzled-word-element');
+    puzzledWordEl.innerText = this.currentPuzzledWord.word;
+    gameWrapper.append(puzzledWordEl);
+
+    const wordTranslationBlock = document.createElement('div');
+    wordTranslationBlock.classList.add('words-translations-block');
+    this.currentWordsTranslations.forEach((el) => this.writeWordsTranslations(el, wordTranslationBlock))
+    wordTranslationBlock.addEventListener('click', this.checkAnswer.bind(this))
+    gameWrapper.append(wordTranslationBlock);
+  }
+
+  writeWordsTranslations(el, block) {
+    const wordTranslationEl = document.createElement('div');
+    wordTranslationEl.classList.add('words-translations-element')
+    wordTranslationEl.innerText = el;
+    block.append(wordTranslationEl);
+  }
+
+  checkAnswer(event) {
+    if(event.target.innerText === this.currentPuzzledWord.wordTranslate) {
+      console.log( this.currentPuzzledWord.wordTranslate)
+      event.target.classList.add('correct')
+    } else {
+      event.target.classList.add('wrong')
+    }
+  }
+
+  startGame() {
+    this.round = 0;
+    document.body.innerHTML = '';
+    this.getRoundData();
+
+  }
+
+  nextRound () {
+    this.round += 1;
+    this.getRoundData();
+  }
+
+
+  endRoundHandler () {
+    if (this.round = 10) {
+      this.endGame();
+    }
+
+    this.nextRound ();
   }
 }
