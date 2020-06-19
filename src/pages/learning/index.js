4f84ad0c5daa97510e5c19242e5001ac4af05959
Iuliia Plekhanova <julec2006@yandex.ru>
2020-06-19 19:49:02 +0400

    add: swiper

--- src/pages/learning/index.js
+++ src/pages/learning/index.js
@@ -1,3 +1,16 @@
 import './index.scss';
 import 'materialize-css';
 import Swiper from 'swiper';
+
+const swiper = new Swiper('.swiper-container', {
+  pagination: {
+    slidesPerView: 1,
+    spaceBetween: 50,
+    el: '.swiper-pagination',
+    type: 'fraction',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
