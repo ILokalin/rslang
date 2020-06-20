994b11a0c10fba5c51fc19cce4f6be0309d68ad8
Iuliia Plekhanova <julec2006@yandex.ru>
2020-06-20 13:34:54 +0400

    feat: add class Card

--- src/pages/learning/components/helpers.js
+++ src/pages/learning/components/helpers.js
@@ -1,4 +1,4 @@
-const measureWordWidth = (word) => {
+export const measureWordWidth = (word) => {
   const canvas = document.createElement('canvas');
   const ctx = canvas.getContext('2d');
   ctx.font = 'bold 2rem serif';
--- src/pages/learning/index.js
+++ src/pages/learning/index.js
@@ -1,30 +1,10 @@
 import './index.scss';
 import 'materialize-css';
-import Swiper from 'swiper';
+import Card from './components/Card';
 
 M.AutoInit();
-
-const swiper = new Swiper('.swiper-container', {
-  slidesPerView: 1,
-  centeredSlides: true,
-  watchSlidesProgress: true,
-  effect: 'fade',
-  pagination: { 
-    el: '.swiper-pagination',
-    type: 'fraction',
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-   a11y: {
-    prevSlideMessage: 'Previous slide',
-    nextSlideMessage: 'Next slide',
-  },
-});
-
-const options = {
+const sideNavOptions = {
   edge: 'right',
 }
+const sideNav = M.Sidenav.init(document.querySelector('.sidenav'), sideNavOptions);
 
-var sideNav = M.Sidenav.init(document.querySelector('.sidenav'), options);
