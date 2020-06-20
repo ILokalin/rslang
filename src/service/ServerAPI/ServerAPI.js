2c17e10952b18b1ee36bc2f5f2cd8c8e1d4ae480
ILokalin <51790923+ILokalin@users.noreply.github.com>
2020-06-20 20:45:56 +0500

    feat(serverAPI): update module as controller between Games and Server

--- src/service/ServerAPI/ServerAPI.js
+++ src/service/ServerAPI/ServerAPI.js
@@ -1,11 +1,7 @@
 /* eslint-disable class-methods-use-this */
 import { AuthPopup } from 'Components/AuthPopup';
 
-const API_URL = 'https://afternoon-falls-25894.herokuapp.com/';
-const API_USERS = 'users';
-const API_SETTINGS = 'settings';
-const API_SIGNIN = 'signin';
-const API_WORDS = 'words';
+
 
 export class ServerAPI {
 
@@ -74,44 +70,15 @@ export class ServerAPI {
     });
   }
 
-  apiUserSettingsPut(store) {
-    return new Promise((resolve, reject) => {
-      fetch(`${API_URL}${API_USERS}/${localStorage.userId}/${API_SETTINGS}`, {
-        method: 'PUT',
-        headers: {
-          Authorization: `Bearer ${localStorage.token}`,
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(store),
-      })
-        .then((rawResponse) => {
-          if (!this.isSuccess(rawResponse)) {
-            reject(this.packError(rawResponse));
-          }
-          return rawResponse.json();
-        })
-        .then((response) => {
-          resolve(response);
-        });
-    });
-  }
+  
 
-  apiGetWords(wordsQuery) {
+  apiGetWords({ group = 0, page = 0} = {}) {
     const rejectErrorReport = (errorReport) => {
       reject(errorReport);
     };
 
     return new Promise((resolve, reject) => {
-      fetch(API_URL + API_WORDS, {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${localStorage.token}`,
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(wordsQuery),
-      })
+      fetch(`${API_URL}${API_WORDS}?group=${group}&page=${page}`)
         .then(
           (rawResponse) => {
             if (!this.isSuccess(rawResponse)) {
@@ -126,94 +93,12 @@ export class ServerAPI {
     })
   }
 
-  apiUserSettingsGet() {
-    return new Promise((resolve, reject) => {
-      fetch(`${API_URL}${API_USERS}/${localStorage.userId}/${API_SETTINGS}`, {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${localStorage.token}`,
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-      })
-        .then((rawResponse) => {
-          if (this.isSuccess(rawResponse)) {
-            return rawResponse.json();
-          }
-          reject(this.packError(rawResponse));
-        })
-        .then((response) => {
-          resolve(response);
-        });
-    });
-  }
-
-  apiUserCreate(user) {
-    return new Promise((resolve, reject) => {
-      fetch(API_URL + API_USERS, {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(user),
-      })
-        .then((rawResponse) => {
-          if (!this.isSuccess(rawResponse)) {
-            reject(this.packError(rawResponse));
-          }
-          return rawResponse.json();
-        })
-        .then((response) => {
-          const { id, email } = response;
-          localStorage.setItem('userId', id);
-          localStorage.setItem('email', email);
-          resolve(response);
-        });
-    });
-  }
-
-  apiUserSignIn(user) {
-    return new Promise((resolve, reject) => {
-      fetch(API_URL + API_SIGNIN, {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(user),
-      })
-        .then((rawResponse) => {
-          if (!this.isSuccess(rawResponse)) {
-            reject(this.packError(rawResponse));
-          }
-          return rawResponse.json();
-        })
-        .then((response) => {
-          const { userId, token } = response;
-          localStorage.setItem('userId', userId);
-          localStorage.setItem('token', token);
-          resolve(response);
-        });
-    });
-  }
-
+  
   checkToken() {
     const { userId, token } = localStorage;
     if (userId && token) {
       return true;
     }
     return false;
-  }
-
-  isSuccess(response) {
-    return response.status >= 200 && response.status < 300;
-  }
-
-  packError(response) {
-    return {
-      status: response.status,
-      message: response.statusText,
-    };
   }
 }
