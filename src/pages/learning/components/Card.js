bad0e45950b2f247e8231e817748c90d09afc04d
Iuliia Plekhanova <julec2006@yandex.ru>
2020-06-20 13:34:54 +0400

    feat: add class Card

--- src/pages/learning/components/Card.js
+++ src/pages/learning/components/Card.js
@@ -1,7 +1,42 @@
+import ElementGen from 'Src/service/DomGen/DomGen';
+import measureWordWidth from './helpers';
+import {mySwiper, wordContentUrl, divider} from './constants';
+
 export default class Card {
   constructor(wordState) {
     this.wordState = wordState;
+    this.cardElem = ElementGen('div', 'swiper-slide card large');
   }
+
+  createCardImage () {
+    const imageContainer = ElementGen('div', 'card-image', this.cardElem);
+    const image = ElementGen('img', 'image-association', imageContainer);
+    image.setAttribute('src', wordContentUrl + this.wordState.image);
+    const imagePlaceholder = ElementGen('div', 'img-placeholder', imageContainer);
+    return imageContainer;
+  }
+
+  createCardContent () {
+    const cardContent = ElementGen('div', 'card-content', this.cardElem);
+    const input = ElementGen('input', 'card-content', cardContent);
+    cardContent.appendChild(divider);
+    const translation = ElementGen('p', 'translation', cardContent);
+    cardContent.appendChild(divider);
+    const explanation = ElementGen('p', 'explanation', cardContent);
+    const explanationTranslation = ElementGen('p', 'explanation-translation', cardContent);
+    cardContent.appendChild(divider);
+
+    const example = ElementGen('p', 'example', cardContent);
+    const exampleTranslation = ElementGen('p', 'example-translation', cardContent);
+    
+
+  }
+
+  createCardAction() {
+
+  }
+
+
 
   
 }
---
+++ src/pages/learning/components/constants.js
@@ -0,0 +1,26 @@
+import Swiper from 'swiper';
+import ElementGen from 'Src/service/DomGen/DomGen';
+
+const mySwiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  centeredSlides: true,
+  watchSlidesProgress: true,
+  effect: 'fade',
+  pagination: { 
+    el: '.swiper-pagination',
+    type: 'fraction',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+   a11y: {
+    prevSlideMessage: 'Previous slide',
+    nextSlideMessage: 'Next slide',
+  },
+});
+
+const wordContentUrl = 'https://raw.githubusercontent.com/jules0802/rslang-data/master/'
+const divider = ElementGen('div', 'divider');
+
+export {mySwiper}
