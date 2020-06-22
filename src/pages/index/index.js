43c338a114f4ec54613b13947e91c44f953b6619
Iuliia Plekhanova <julec2006@yandex.ru>
2020-06-22 16:08:38 +0400

    feat: add header and side menu to main page

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,5 +1,5 @@
 import './index.scss';
-
+import 'materialize-css';
 require.context('Src', true, /\.(png|svg|jpg|gif|mp3)$/);
 
 // eslint-disable-next-line no-console
@@ -8,3 +8,5 @@ console.log(
   'font-family: sans-serif; font-size: 28px; letter-spacing: 0.1em;',
   'RS School, group 22 2020q1',
 );
+
+ M.AutoInit();
