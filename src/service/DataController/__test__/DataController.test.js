beb9cb074a68cfc7c5e056f5eeb6248896872955
ILokalin <51790923+ILokalin@users.noreply.github.com>
2020-06-24 03:42:10 +0500

    feat(dataControl): tests for getWords: 100, 500, 100 words per request

--- src/service/DataController/__test__/DataController.test.js
+++ src/service/DataController/__test__/DataController.test.js
@@ -1,4 +1,4 @@
-// import LocalStorageMock from './LocalStorageMock';
+import LocalStorageMock from './LocalStorageMock';
 import { DataController } from '../DataController';
 
 global.fetch = require('node-fetch');
@@ -10,64 +10,81 @@ const testUser = {
   password: 'checkerCH#2',
 };
 
-test('unpack create normal object', () => {
-  const optionalTest = {
-    name: '"Tester Testovich"',
-    settings:
-      '{"lastTrain":"date","cardsPerDay":30,"newCardsPerDay":15,"justNewWords":0,"cardContainsTranslation":1,"cardContainsMeaning":0,"cardContainsMeaningTransl":0,"cardContainsExample":0,"cardContainsExampleTransl":0,"cardContainsPicture":1,"cardContainsTranscription":1,"footerBtnsEnabled":1,"deleteBtnEnabled":1,"showAnswerBtnEnabled":0,"autoPlayEnabled":1}',
-  };
+describe('Helpers tests', () => {
+  test('unpack create normal object', () => {
+    const optionalTest = {
+      name: '"Tester Testovich"',
+      settings:
+        '{"lastTrain":"date","cardsPerDay":30,"newCardsPerDay":15,"justNewWords":0,"cardContainsTranslation":1,"cardContainsMeaning":0,"cardContainsMeaningTransl":0,"cardContainsExample":0,"cardContainsExampleTransl":0,"cardContainsPicture":1,"cardContainsTranscription":1,"footerBtnsEnabled":1,"deleteBtnEnabled":1,"showAnswerBtnEnabled":0,"autoPlayEnabled":1}',
+    };
 
-  const expectResult = {
-    name: 'Tester Testovich',
-    settings: {
-      lastTrain: 'date',
-      cardsPerDay: 30,
-      newCardsPerDay: 15,
-      justNewWords: 0,
-      cardContainsTranslation: 1,
-      cardContainsMeaning: 0,
-      cardContainsMeaningTransl: 0,
-      cardContainsExample: 0,
-      cardContainsExampleTransl: 0,
-      cardContainsPicture: 1,
-      cardContainsTranscription: 1,
-      footerBtnsEnabled: 1,
-      deleteBtnEnabled: 1,
-      showAnswerBtnEnabled: 0,
-      autoPlayEnabled: 1,
-    },
-  };
+    const expectResult = {
+      name: 'Tester Testovich',
+      settings: {
+        lastTrain: 'date',
+        cardsPerDay: 30,
+        newCardsPerDay: 15,
+        justNewWords: 0,
+        cardContainsTranslation: 1,
+        cardContainsMeaning: 0,
+        cardContainsMeaningTransl: 0,
+        cardContainsExample: 0,
+        cardContainsExampleTransl: 0,
+        cardContainsPicture: 1,
+        cardContainsTranscription: 1,
+        footerBtnsEnabled: 1,
+        deleteBtnEnabled: 1,
+        showAnswerBtnEnabled: 0,
+        autoPlayEnabled: 1,
+      },
+    };
 
-  expect(dataController.unpackUserSettings(optionalTest)).toEqual(expectResult);
+    expect(dataController.unpackUserSettings(optionalTest)).toEqual(expectResult);
+  });
+  test('pack converts object fields to strings', () => {
+    const optionalTest = {
+      name: 'Tester Testovich',
+      settings: {
+        lastTrain: 'date',
+        cardsPerDay: 30,
+        newCardsPerDay: 15,
+        justNewWords: 0,
+        cardContainsTranslation: 1,
+        cardContainsMeaning: 0,
+        cardContainsMeaningTransl: 0,
+        cardContainsExample: 0,
+        cardContainsExampleTransl: 0,
+        cardContainsPicture: 1,
+        cardContainsTranscription: 1,
+        footerBtnsEnabled: 1,
+        deleteBtnEnabled: 1,
+        showAnswerBtnEnabled: 0,
+        autoPlayEnabled: 1,
+      },
+    };
+
+    const expectResult = {
+      name: '"Tester Testovich"',
+      settings:
+        '{"lastTrain":"date","cardsPerDay":30,"newCardsPerDay":15,"justNewWords":0,"cardContainsTranslation":1,"cardContainsMeaning":0,"cardContainsMeaningTransl":0,"cardContainsExample":0,"cardContainsExampleTransl":0,"cardContainsPicture":1,"cardContainsTranscription":1,"footerBtnsEnabled":1,"deleteBtnEnabled":1,"showAnswerBtnEnabled":0,"autoPlayEnabled":1}',
+    };
+
+    expect(dataController.packUserSettings(optionalTest)).toEqual(expectResult);
+  });
 });
 
-test('pack converts object fields to strings', () => {
-  const optionalTest = {
-    name: 'Tester Testovich',
-    settings: {
-      lastTrain: 'date',
-      cardsPerDay: 30,
-      newCardsPerDay: 15,
-      justNewWords: 0,
-      cardContainsTranslation: 1,
-      cardContainsMeaning: 0,
-      cardContainsMeaningTransl: 0,
-      cardContainsExample: 0,
-      cardContainsExampleTransl: 0,
-      cardContainsPicture: 1,
-      cardContainsTranscription: 1,
-      footerBtnsEnabled: 1,
-      deleteBtnEnabled: 1,
-      showAnswerBtnEnabled: 0,
-      autoPlayEnabled: 1,
-    },
-  };
+describe('Interface methods', () => {
+  test('getWords gets custom words per page', async () => {
+    await expect(dataController.getWords({ wordsPerPage: 100 })).resolves.toHaveLength(100);
+  });
 
-  const expectResult = {
-    name: '"Tester Testovich"',
-    settings:
-      '{"lastTrain":"date","cardsPerDay":30,"newCardsPerDay":15,"justNewWords":0,"cardContainsTranslation":1,"cardContainsMeaning":0,"cardContainsMeaningTransl":0,"cardContainsExample":0,"cardContainsExampleTransl":0,"cardContainsPicture":1,"cardContainsTranscription":1,"footerBtnsEnabled":1,"deleteBtnEnabled":1,"showAnswerBtnEnabled":0,"autoPlayEnabled":1}',
-  };
+  test('getWords gets 500 words from group 0 page 0, after previus request', async () => {
+    await expect(dataController.getWords({ wordsPerPage: 500 })).resolves.toHaveLength(500);
+  });
 
-  expect(dataController.packUserSettings(optionalTest)).toEqual(expectResult);
+  test('getWords gets 100 words from group 5', async () => {
+    await expect(dataController.getWords({ wordsPerPage: 100, group: 5 })).resolves.toHaveLength(
+      100,
+    );
+  });
 });
