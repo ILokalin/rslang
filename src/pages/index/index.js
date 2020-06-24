79905961337200c8b85e64d9fea10f4655eea5af
Iuliia Plekhanova <julec2006@yandex.ru>
2020-06-24 23:33:24 +0400

    feat: add vocabulary layout

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,6 +1,7 @@
 import './index.scss';
 import 'materialize-css';
 import './js/components/buttons';
+import './js/components/sideMenu';
 import {whoIsGameFor} from './js/components/buttons';
 import './js/components/form-components';
 import {renderShortTermStat} from './js/components/mainPageStat';
@@ -10,17 +11,5 @@ require.context('Src', true, /\.(png|svg|jpg|gif|mp3)$/);
 
 // eslint-disable-next-line no-undef
 M.AutoInit();
-
-
-document.querySelector('.menu__settings').addEventListener('click', () => {
-  document.querySelector('.settings-container').classList.remove('hidden');
-  document.querySelector('main').classList.add('hidden');
-})
-
-document.querySelector('.menu__home').addEventListener('click', () => {
-  document.querySelector('.settings-container').classList.add('hidden');
-  document.querySelector('main').classList.remove('hidden');
-})
-
 renderShortTermStat();
 renderStatChart();
