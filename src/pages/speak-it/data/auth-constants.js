b2af7447b181152d084c02cec7e7b494d900de0e
Anastasiya Sidarovich <anasidorovich@gmail.com>
2020-06-24 20:54:32 +0300

    feat(auth): add authorization functionality

---
+++ src/pages/speak-it/data/auth-constants.js
@@ -0,0 +1,6 @@
+const logInBtn = document.querySelector('.login-btn ');
+const signUpBtn = document.querySelector('.signup-btn');
+const registerMsgEl = document.querySelector('.register-msg');
+const registerBtn = document.querySelector('.register-btn');
+
+export { logInBtn, signUpBtn, registerMsgEl, registerBtn };
