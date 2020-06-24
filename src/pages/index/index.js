a8a57a0a98d3b8438f0b441204b4b7f2cad2684c
Iuliia Plekhanova <julec2006@yandex.ru>
2020-06-24 13:56:53 +0400

    fix: home page links

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -10,4 +10,11 @@ M.AutoInit();
 
 document.querySelector('.menu__settings').addEventListener('click', () => {
   document.querySelector('.settings-container').classList.remove('hidden');
-});
+  document.querySelector('main').classList.add('hidden');
+})
+
+document.querySelector('.menu__home').addEventListener('click', () => {
+  document.querySelector('.settings-container').classList.add('hidden');
+  document.querySelector('main').classList.remove('hidden');
+})
+
