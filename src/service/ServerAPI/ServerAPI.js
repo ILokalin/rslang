14e873b2ea3b922c69381d6707e7ec4918076ee2
ILokalin <51790923+ILokalin@users.noreply.github.com>
2020-06-27 01:52:21 +0500

    refactor: correct file name

--- src/service/ServerAPI/ServerAPI.js
+++ src/service/ServerAPI/ServerAPI.js
@@ -1,50 +1,229 @@
-/* eslint-disable class-methods-use-this */
-const API_URL = 'https://afternoon-falls-25894.herokuapp.com/';
-const API_USERS = 'users';
-const API_SETTINGS = 'settings';
-const API_SIGNIN = 'signin';
-const API_WORDS = 'words';
+import { api } from './SereverApiConst';
 
-export class ServerAPI {
-  logoutUser() {
-    localStorage.setItem('token', '');
-  }
+const defaultWordsPerExample = 100;
 
-  getWords({ group = 0, page = 0 } = {}) {
-    const rejectErrorReport = (errorReport) => {
-      reject(errorReport);
-    };
+function isSuccess(response) {
+  return response.status >= 200 && response.status < 300;
+}
 
-    return new Promise((resolve, reject) => {
-      fetch(`${API_URL}${API_WORDS}?group=${group}&page=${page}`)
-        .then((rawResponse) => {
-          if (!this.isSuccess(rawResponse)) {
-            reject(this.packError(rawResponse));
-          }
+export function apiUserWordsGetAll() {
+  return new Promise((resolve, reject) => {
+    fetch(`${api.url}${api.users}/${localStorage.userId}/${api.words}`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${localStorage.token}`,
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((rawResponse) => {
+        if (isSuccess(rawResponse)) {
           return rawResponse.json();
-        })
-        .then((response) => {
-          resolve(response);
-        });
-    });
+        }
+        const error = new Error(rawResponse.statusText);
+        error.master = 'words';
+        error.code = rawResponse.status;
+        throw error;
+      })
+      .then((response) => {
+        resolve(response);
+      })
+      .catch((errorReport) => reject(errorReport));
+  });
+}
+
+export function apiUserWordsGet(wordId) {
+  return new Promise((resolve, reject) => {
+    fetch(`${api.url}${api.users}/${localStorage.userId}/${api.words}/${wordId}`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${localStorage.token}`,
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((rawResponse) => {
+        if (isSuccess(rawResponse)) {
+          return rawResponse.json();
+        }
+        const error = new Error(rawResponse.statusText);
+        error.master = 'words';
+        error.code = rawResponse.status;
+        throw error;
+      })
+      .then((response) => {
+        resolve(response);
+      })
+      .catch((errorReport) => reject(errorReport));
+  });
+}
+
+export function apiUserWordsSave(wordId, wordData, method) {
+  return new Promise((resolve, reject) => {
+    fetch(`${api.url}${api.users}/${localStorage.userId}/${api.words}/${wordId}`, {
+      method,
+      headers: {
+        Authorization: `Bearer ${localStorage.token}`,
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(wordData),
+    })
+      .then((rawResponse) => {
+        if (isSuccess(rawResponse)) {
+          return rawResponse.json();
+        }
+        const error = new Error(rawResponse.statusText);
+        error.master = 'words';
+        error.code = rawResponse.status;
+        throw error;
+      })
+      .then((response) => {
+        resolve(response);
+      })
+      .catch((errorReport) => reject(errorReport));
+  });
+}
+
+export function apiGetWords(requestData) {
+  const { page, group, wordsPerExampleSentenceLTE = '', wordsPerPage = '' } = requestData;
+
+  let wordsPerExampleString = wordsPerExampleSentenceLTE
+    ? `&wordsPerExampleSentenceLTE=${wordsPerExampleSentenceLTE}`
+    : '';
+  const wordsPerPageString = wordsPerPage ? `&wordsPerPage=${wordsPerPage}` : '';
+
+  if (wordsPerPage !== '' && wordsPerExampleSentenceLTE === '') {
+    wordsPerExampleString = `&wordsPerExampleSentenceLTE=${defaultWordsPerExample}`;
   }
 
-  checkToken() {
-    const { userId, token } = localStorage;
-    if (userId && token) {
-      return true;
-    }
-    return false;
-  }
+  return new Promise((resolve, reject) => {
+    fetch(
+      `${api.url}${api.words}?group=${group}&page=${page}${wordsPerExampleString}${wordsPerPageString}`,
+    )
+      .then((rawResponse) => {
+        if (isSuccess(rawResponse)) {
+          return rawResponse.json();
+        }
+        const error = new Error(rawResponse.statusText);
+        error.master = 'words';
+        error.code = rawResponse.status;
+        throw error;
+      })
+      .then((response) => {
+        resolve(response);
+      })
+      .catch((errorReport) => reject(errorReport));
+  });
+}
 
-  isSuccess(response) {
-    return response.status >= 200 && response.status < 300;
-  }
+export function apiUserSettingsPut(userSettingsUpload) {
+  return new Promise((resolve, reject) => {
+    fetch(`${api.url}${api.users}/${localStorage.userId}/${api.settings}`, {
+      method: 'PUT',
+      headers: {
+        Authorization: `Bearer ${localStorage.token}`,
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userSettingsUpload),
+    })
+      .then((rawResponse) => {
+        if (isSuccess(rawResponse)) {
+          return rawResponse.json();
+        }
+        const error = new Error(rawResponse.statusText);
+        error.master = 'userSettings';
+        error.code = rawResponse.status;
+        throw error;
+      })
+      .then((userSettings) => {
+        resolve(userSettings);
+      })
+      .catch((errorReport) => reject(errorReport));
+  });
+}
 
-  packError(response) {
-    return {
-      status: response.status,
-      message: response.statusText,
-    };
-  }
+export function apiUserSettingsGet() {
+  return new Promise((resolve, reject) => {
+    fetch(`${api.url}${api.users}/${localStorage.userId}/${api.settings}`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${localStorage.token}`,
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((rawResponse) => {
+        if (isSuccess(rawResponse)) {
+          return rawResponse.json();
+        }
+        const error = new Error(rawResponse.statusText);
+        error.master = 'userSettings';
+        error.code = rawResponse.status;
+        throw error;
+      })
+      .then((response) => {
+        resolve(response);
+      })
+      .catch((errorReport) => reject(errorReport));
+  });
+}
+
+export function apiUserCreate(user) {
+  return new Promise((resolve, reject) => {
+    fetch(api.url + api.users, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    })
+      .then((rawResponse) => {
+        if (isSuccess(rawResponse)) {
+          return rawResponse.json();
+        }
+        const error = new Error(rawResponse.statusText);
+        error.master = 'user';
+        error.code = rawResponse.status;
+        throw error;
+      })
+      .then((response) => {
+        const { id, email } = response;
+        localStorage.setItem('userId', id);
+        localStorage.setItem('email', email);
+        resolve(response);
+      })
+      .catch((errorReport) => reject(errorReport));
+  });
+}
+
+export function apiUserSignIn(user) {
+  return new Promise((resolve, reject) => {
+    fetch(api.url + api.signin, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    })
+      .then((rawResponse) => {
+        if (isSuccess(rawResponse)) {
+          return rawResponse.json();
+        }
+        const error = new Error(rawResponse.statusText);
+        error.master = 'signIn';
+        error.code = rawResponse.status;
+        throw error;
+      })
+      .then((response) => {
+        const { userId, token } = response;
+        localStorage.setItem('userId', userId);
+        localStorage.setItem('token', token);
+        resolve(response);
+      })
+      .catch((errorReport) => reject(errorReport));
+  });
 }
