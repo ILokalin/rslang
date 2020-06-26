85fe7f2b24843d73f56604d14ed841946c8c192d
ILokalin <51790923+ILokalin@users.noreply.github.com>
2020-06-27 01:46:54 +0500

    feat(authPopup): implement registration form and toggle it with login form

--- src/components/AuthPopup/AuthPopup.js
+++ src/components/AuthPopup/AuthPopup.js
@@ -1,150 +1,24 @@
-import { DomGen } from 'Service/DomGen';
-import { closeAuthPopup, authPopupState, setUserData, authReportStore } from 'Service/AppState';
-import { AuthPopConst } from './AuthPopupConst';
-import 'materialize-css';
+import "materialize-css";
+import { DomGen } from "Service/DomGen";
+import {
+  closeAuthPopup,
+  authPopupState,
+  setUserData,
+  authReportStore,
+} from "Service/AppState";
+import { AuthPopConst } from "./AuthPopupConst";
+import { AuthPopupForm } from "./AuthPopupForm";
 
 export class AuthPopup {
   constructor() {
     this.sendUserData = this.sendUserData.bind(this);
+    this.formToLogin = this.formToLogin.bind(this);
+    this.formToRegister = this.formToRegister.bind(this);
     this.body = document.body;
   }
 
   init() {
-    this.popup = DomGen({
-      name: 'auth-popup',
-      tag: 'div',
-      children: [
-        {
-          tag: 'div',
-          className: 'authorization',
-          children: [
-            {
-              tag: 'div',
-              id: 'login-page',
-              classAdd: 'row',
-              children: [
-                {
-                  tag: 'div',
-                  classAdd: 'col,s12,z-depth-6,card-panel',
-                  children: [
-                    { tag: 'h2', classAdd: 'center-align', innerText: 'Login/Register' },
-                    { tag: 'p', classAdd: 'center-align', isAccess: 'reportLine' },
-                    {
-                      tag: 'form',
-                      classAdd: 'login-form',
-                      children: [
-                        { tag: 'div', classAdd: 'row' },
-                        {
-                          tag: 'div',
-                          classAdd: 'row',
-                          children: [
-                            {
-                              tag: 'div',
-                              classAdd: 'input-field,col,s12',
-                              children: [
-                                {
-                                  tag: 'i',
-                                  classAdd: 'material-icons,prefix',
-                                  innerText: 'mail_outline',
-                                },
-                                {
-                                  tag: 'input',
-                                  classAdd: 'validate',
-                                  id: 'email',
-                                  type: 'email',
-                                  isAccess: 'email',
-                                },
-                                {
-                                  tag: 'label',
-                                  for: 'email',
-                                  dataError: 'wrong',
-                                  dataSuccess: 'right',
-                                  innerText: 'Email',
-                                },
-                              ],
-                            },
-                          ],
-                        },
-                        {
-                          tag: 'div',
-                          classAdd: 'row',
-                          children: [
-                            {
-                              tag: 'div',
-                              classAdd: 'input-field,col,s12',
-                              children: [
-                                {
-                                  tag: 'i',
-                                  classAdd: 'material-icons,prefix',
-                                  innerText: 'lock_outline',
-                                },
-                                {
-                                  tag: 'input',
-                                  classAdd: 'validate',
-                                  id: 'password',
-                                  type: 'password',
-                                  isAccess: 'password',
-                                },
-                                {
-                                  tag: 'label',
-                                  for: 'password',
-                                  dataError: 'wrong',
-                                  dataSuccess: 'right',
-                                  innerText: 'Password',
-                                },
-                              ],
-                            },
-                          ],
-                        },
-                        {
-                          tag: 'div',
-                          classAdd: 'row',
-                          children: [
-                            {
-                              tag: 'div',
-                              classAdd: 'input-field,col,s12',
-                              children: [
-                                {
-                                  tag: 'button',
-                                  type: 'button',
-                                  href: '#',
-                                  classAdd: 'login-btn,btn,waves-effect,waves-light,col,s3,offset-s6',
-                                  innerText: 'Login',
-                                  isAccess: 'login',
-                                },
-                                {
-                                  tag: 'button',
-                                  type: 'button',
-                                  href: '#',
-                                  classAdd: 'cancel-btn,btn,waves-effect,waves-light,teal,lighten-2,col,s3',
-                                  innerText: 'Cancel',
-                                  isAccess: 'cancel',
-                                },
-                              ],
-                            },
-                          ],
-                        },
-                        // <div class="row">
-                        //       <div class="error-message"></div>
-                        //   </div>
-                        //   <div class="row">
-                        //       <div class="input-field col s6 m6 l6">
-                        //           <p class="margin medium-small register-msg">No account?</p>
-                        //           <p class="margin medium-small register-btn"><a href="">Register Now!</a></p>
-                        //       </div>
-                        //   </div>
-                      ],
-                    },
-                  ],
-                },
-              ],
-            },
-
-            //         'the password must contain at least one lowercase character, one uppercase, one special character, one digit',
-          ],
-        },
-      ],
-    });
+    this.popup = DomGen(AuthPopupForm);
 
     authReportStore.watch((reportMessage) => {
       this.popup.reportLine.innerText = reportMessage;
@@ -158,12 +32,53 @@ export class AuthPopup {
       }
     });
 
-    this.popup.cancel.addEventListener('click', this.cancelAuth);
-    this.popup.login.addEventListener('click', this.sendUserData);
-    // this.popup.register.addEventListener('click', this.showRegisterForm);
+    this.popup.cancel.addEventListener("click", this.cancelAuth);
+    this.popup.login.addEventListener("click", this.sendUserData);
+    this.popup.toggleLogin.addEventListener("click", this.formToLogin);
+    this.popup.toggleRegister.addEventListener("click", this.formToRegister);
+    this.popup.register.addEventListener("click", this.sendUserData);
+  }
+
+  formToLogin() {
+    const {
+      header,
+      nameLine,
+      register,
+      login,
+      toggleBlockLogin,
+      toggleBlockRegister,
+    } = this.popup;
+
+    header.innerText = "Login";
+    nameLine.style.display = "none";
+    register.style.display = "none";
+    login.style.display = "block";
+    toggleBlockLogin.style.display = "none";
+    toggleBlockRegister.style.display = "block";
+    this.isformRegister = false;
+  }
+
+  formToRegister() {
+    const {
+      header,
+      nameLine,
+      register,
+      login,
+      toggleBlockLogin,
+      toggleBlockRegister,
+    } = this.popup;
+
+    header.innerText = "Register";
+    nameLine.style.display = "block";
+    register.style.display = "block";
+    login.style.display = "none";
+    toggleBlockLogin.style.display = "block";
+    toggleBlockRegister.style.display = "none";
+    this.isformRegister = true;
   }
 
   openPopup() {
+    this.formToLogin();
     this.body.append(this.popup.block);
   }
 
@@ -177,26 +92,34 @@ export class AuthPopup {
 
   sendUserData() {
     const isValidate =
-      AuthPopConst.EMAIL_REGEXP.test(this.popup.email.value) &&
-      AuthPopConst.PASSWORD_REGEXP.test(this.popup.password.value);
+      AuthPopConst.emailRegexp.test(this.popup.email.value) &&
+      AuthPopConst.passwordRegexp.test(this.popup.password.value) &&
+      AuthPopConst.nameRegexp.test(this.popup.name.value);
+
     const user = {
       email: this.popup.email.value,
       password: this.popup.password.value,
     };
 
+    if (this.isformRegister) {
+      user.statusRegister = this.isformRegister;
+      user.name = this.popup.name.value;
+    }
+
     if (isValidate) {
       setUserData(user);
     } else {
       if (!AuthPopConst.EMAIL_REGEXP.test(this.popup.email.value)) {
-        this.popup.reportLine.innerText = 'Please input correct email address';
+        this.popup.reportLine.innerText = "Please input correct email address";
       }
       if (!AuthPopConst.PASSWORD_REGEXP.test(this.popup.password.value)) {
-        this.popup.reportLine.innerText = 'Please use correct password format. See below.';
+        this.popup.reportLine.innerText =
+          "Please use correct password format. See below.";
+      }
+      if (!AuthPopConst.nameRegexp.test(this.popup.name.value)) {
+        this.popup.reportLine.innerText =
+          "Please use one or more letters for name";
       }
     }
-  }
-
-  showRegisterForm() {
-    this.popup.reportLine.innerText = 'development in progress';
   }
 }
---
+++ src/components/AuthPopup/AuthPopupForm.js
@@ -0,0 +1,226 @@
+export const AuthPopupForm = {
+  name: 'auth-popup',
+  tag: 'div',
+  children: [
+    {
+      tag: 'div',
+      className: 'authorization',
+      children: [
+        {
+          tag: 'div',
+          id: 'login-page',
+          classAdd: 'row',
+          children: [
+            {
+              tag: 'div',
+              classAdd: 'col,s12,z-depth-6,card-panel',
+              children: [
+                {
+                  tag: 'h2',
+                  classAdd: 'center-align',
+                  innerText: 'Login',
+                  isAccess: 'header',
+                },
+                {
+                  tag: 'p',
+                  classAdd: 'center-align,error-message',
+                  isAccess: 'reportLine',
+                },
+                {
+                  tag: 'form',
+                  classAdd: 'login-form',
+                  children: [
+                    {
+                      tag: 'div',
+                      classAdd: 'row,input-wrapper',
+                      children: [
+                        {
+                          tag: 'div',
+                          classAdd: 'input-field,first-line,col,s12',
+                          children: [
+                            {
+                              tag: 'i',
+                              classAdd: 'material-icons,prefix',
+                              innerText: 'mail_outline',
+                            },
+                            {
+                              tag: 'input',
+                              classAdd: 'validate',
+                              id: 'email',
+                              type: 'email',
+                              isAccess: 'email',
+                            },
+                            {
+                              tag: 'label',
+                              for: 'email',
+                              dataError: 'wrong',
+                              dataSuccess: 'right',
+                              innerText: 'Email',
+                            },
+                          ],
+                        },
+                      ],
+                    },
+                    {
+                      tag: 'div',
+                      classAdd: 'row,input-wrapper',
+                      isAccess: 'nameLine',
+                      children: [
+                        {
+                          tag: 'div',
+                          classAdd: 'input-field,first-line,col,s12',
+                          children: [
+                            {
+                              tag: 'i',
+                              classAdd: 'material-icons,prefix',
+                              innerText: 'person_outline',
+                            },
+                            {
+                              tag: 'input',
+                              classAdd: 'validate',
+                              id: 'name',
+                              type: 'text',
+                              isAccess: 'name',
+                            },
+                            {
+                              tag: 'label',
+                              for: 'name',
+                              dataError: 'wrong',
+                              dataSuccess: 'right',
+                              innerText: 'Name',
+                            },
+                          ],
+                        },
+                      ],
+                    },
+                    {
+                      tag: 'div',
+                      classAdd: 'row,input-wrapper',
+                      children: [
+                        {
+                          tag: 'div',
+                          classAdd: 'input-field,col,s12',
+                          children: [
+                            {
+                              tag: 'i',
+                              classAdd: 'material-icons,prefix',
+                              innerText: 'lock_outline',
+                            },
+                            {
+                              tag: 'input',
+                              classAdd: 'validate',
+                              id: 'password',
+                              type: 'password',
+                              isAccess: 'password',
+                            },
+                            {
+                              tag: 'label',
+                              for: 'password',
+                              dataError: 'wrong',
+                              dataSuccess: 'right',
+                              innerText: 'Password',
+                            },
+                          ],
+                        },
+                      ],
+                    },
+                    {
+                      tag: 'div',
+                      classAdd: 'row,input-wrapper',
+                      children: [
+                        {
+                          tag: 'div',
+                          classAdd: 'input-field,col,buttons-wrapper,s12',
+                          children: [
+                            {
+                              tag: 'div',
+                              class: 'col s6',
+                              isAccess: 'toggleBlockRegister',
+                              children: [
+                                {
+                                  tag: 'p',
+                                  classAdd: 'margin,medium-small,register-msg',
+                                  innerText: 'No account?',
+                                },
+                                {
+                                  tag: 'p',
+                                  classAdd: 'margin,medium-small,register-btn',
+                                  children: [
+                                    {
+                                      tag: 'button',
+                                      type: 'button',
+                                      className: 'button,button--link',
+                                      innerText: 'Register Now!',
+                                      isAccess: 'toggleRegister',
+                                    },
+                                  ],
+                                },
+                              ],
+                            },
+                            {
+                              tag: 'div',
+                              class: 'col s6',
+                              isAccess: 'toggleBlockLogin',
+                              children: [
+                                {
+                                  tag: 'p',
+                                  classAdd: 'margin,medium-small,register-msg',
+                                  innerText: 'Have account?',
+                                },
+                                {
+                                  tag: 'p',
+                                  classAdd: 'margin,medium-small,register-btn',
+                                  children: [
+                                    {
+                                      tag: 'button',
+                                      type: 'button',
+                                      className: 'button,button--link',
+                                      innerText: 'Sign in!',
+                                      isAccess: 'toggleLogin',
+                                    },
+                                  ],
+                                },
+                              ],
+                            },
+                            {
+                              tag: 'button',
+                              type: 'button',
+                              href: '#',
+                              classAdd: 'login-btn,btn,waves-effect,waves-light,col,s3',
+                              innerText: 'Register',
+                              isAccess: 'register',
+                            },
+                            {
+                              tag: 'button',
+                              type: 'button',
+                              href: '#',
+                              classAdd: 'login-btn,btn,waves-effect,waves-light,col,s3',
+                              innerText: 'Login',
+                              isAccess: 'login',
+                            },
+                            {
+                              tag: 'button',
+                              type: 'button',
+                              href: '#',
+                              className: 'button',
+                              classAdd:
+                                'cancel-btn,btn,waves-effect,waves-light,teal,lighten-2,col,s3',
+                              innerText: 'Cancel',
+                              isAccess: 'cancel',
+                            },
+                          ],
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+
+        //         'the password must contain at least one lowercase character, one uppercase, one special character, one digit',
+      ],
+    },
+  ],
+};
