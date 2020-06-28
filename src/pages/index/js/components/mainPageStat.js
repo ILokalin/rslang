e965c891f710f3efe618e458080755a3115c799a
Iuliia Plekhanova <julec2006@yandex.ru>
2020-06-28 22:21:14 +0400

    fix: shortTermStat beta

--- src/pages/index/js/components/mainPageStat.js
+++ src/pages/index/js/components/mainPageStat.js
@@ -6,6 +6,10 @@ export const renderShortTermStat = () => {
     document.querySelector('.statistics__repeated-words').innerText = shortTermStat.totalCards - shortTermStat.newWords;
     document.querySelector('.statistics__total-cards').innerText = shortTermStat.totalCards;
     document.querySelector('.statistics__correct-in-row').innerText = shortTermStat.longestChain;
+    document.querySelector('.sc-max').innerText = settings.cardsPerDay;
+    document.querySelector('.sc-value').innerText = shortTermStat.totalCards;
+    const angle =  Math.round((180 * (shortTermStat.totalCards || 0) / settings.cardsPerDay * 100) / 100) ;
+    document.querySelector('.sc-percentage').setAttribute('style', `transform: rotate(${angle}deg);`);
   }
   const settings = JSON.parse(localStorage.getItem('settings'));
   console.log(settings);
@@ -13,10 +17,5 @@ export const renderShortTermStat = () => {
     document.querySelector('.plan__new').innerText = settings.newCardsPerDay;
     document.querySelector('.plan_repeat').innerText = settings.cardsPerDay - settings.newCardsPerDay;
     document.querySelector('.plan__total').innerText = settings.cardsPerDay;
-  }
-
-  document.querySelector('.sc-max').innerText = settings.cardsPerDay;
-  document.querySelector('.sc-value').innerText = shortTermStat.totalCards;
-  const angle =  Math.round((180 * (shortTermStat.totalCards || 0) / settings.cardsPerDay * 100) / 100) ;
-  document.querySelector('.sc-percentage').setAttribute('style', `transform: rotate(${angle}deg);`);
+  }  
 }
