06dc574c6bb939599fd0761dc4dfad8c8665971b
ILokalin <51790923+ILokalin@users.noreply.github.com>
2020-06-30 03:14:44 +0500

    feat: add statistic loader

--- src/service/ServerAPI/SereverApiConst.js
+++ src/service/ServerAPI/SereverApiConst.js
@@ -2,6 +2,7 @@ export const api = {
   url: 'https://afternoon-falls-25894.herokuapp.com/',
   users: 'users',
   settings: 'settings',
+  statistics: 'statistics',
   signin: 'signin',
   words: 'words',
   aggregatedWords: 'aggregatedWords',
--- src/service/ServerAPI/ServerAPI.js
+++ src/service/ServerAPI/ServerAPI.js
@@ -7,9 +7,8 @@ function isSuccess(response) {
 }
 
 export function apiUserAggregatedWords(difficultyGroup) {
-  const filter = encodeURIComponent(
-    `{"$or":[${difficultyGroup.map((group) => `{"userWord.difficulty":"${group}"}`)}]}`,
-  );
+  const queryString = difficultyGroup.map((group) => `{"userWord.difficulty":"${group}"}`);
+  const filter = encodeURIComponent(`{"$or":[${queryString}]}`);
   const fetchUrl = `${api.url}${api.users}/${localStorage.userId}/${api.aggregatedWords}?wordsPerPage=3600&filter=${filter}`;
 
   return new Promise((resolve, reject) => {
@@ -148,9 +147,9 @@ export function apiGetWords(requestData) {
   });
 }
 
-export function apiUserSettingsPut(userSettingsUpload) {
+export function apiUserSettingsPut(userSettingsUpload, section = 'settings') {
   return new Promise((resolve, reject) => {
-    fetch(`${api.url}${api.users}/${localStorage.userId}/${api.settings}`, {
+    fetch(`${api.url}${api.users}/${localStorage.userId}/${api[section]}`, {
       method: 'PUT',
       headers: {
         Authorization: `Bearer ${localStorage.token}`,
@@ -175,9 +174,9 @@ export function apiUserSettingsPut(userSettingsUpload) {
   });
 }
 
-export function apiUserSettingsGet() {
+export function apiUserSettingsGet(section = 'settings') {
   return new Promise((resolve, reject) => {
-    fetch(`${api.url}${api.users}/${localStorage.userId}/${api.settings}`, {
+    fetch(`${api.url}${api.users}/${localStorage.userId}/${api[section]}`, {
       method: 'GET',
       headers: {
         Authorization: `Bearer ${localStorage.token}`,
