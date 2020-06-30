bd4ac9b872e0e0f478e932c58fcca840b9352dcd
ANDREI AHUREIKIN <ahureikinandrei@gmail.com>
2020-06-30 11:05:12 +0300

    feat: creat words cards

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -3,12 +3,14 @@ import 'materialize-css';
 import './js/components/buttons';
 import './js/components/sideMenu';
 import './js/components/form-components';
-import {renderShortTermStat} from './js/components/mainPageStat';
-import {renderStatChart} from './js/components/statChart';
+// import { renderShortTermStat } from './js/components/mainPageStat';
+import { renderStatChart } from './js/components/statChart';
+import VocabularyWord from './js/components/vocabulary';
+
+const vocabulary = new VocabularyWord();
 
 require.context('Src', true, /\.(png|svg|jpg|gif|mp3)$/);
 
-// eslint-disable-next-line no-undef
-M.AutoInit();
-renderShortTermStat();
+// renderShortTermStat();
 renderStatChart();
+vocabulary.renderVocabulary();
--- src/pages/index/js/components/vocabulary.js
+++ src/pages/index/js/components/vocabulary.js
@@ -1,39 +1,121 @@
 import { ElementGen } from 'Src/service/DomGen/DomGen';
+import { dataController } from '../constants';
 
-class VocabularyWord {
+const vocabularyDifficult = document.getElementById('vocabulary__difficult');
+const vocabularyOnLearn = document.getElementById('vocabulary__on-learn');
+const vocabularyDeleted = document.getElementById('vocabulary__deleted');
+
+export default class VocabularyWord {
   constructor(wordState) {
     this.wordState = wordState;
-    this.cardElem = ElementGen('li', 'vocabulary__word-container');
-    this.cardElem.appendChild(this.createHeader());
-    this.cardElem.appendChild(this.createBody());
   }
 
-  createHeader () {
+  createHeader() {
     const div = ElementGen('div', 'collapsible-header', this.cardElem);
-    div.insertAdjacentHtml('afterbegin',`<i data-source="${dataController.getMaterials(this.wordState.audio).then((fullPath) => fullPath)}" class="material-icons">volume_up</i>
-                                        <span class="vocabulary__word">${this.wordState.word}</span> 
-                                        <span class="new badge" data-badge-caption="progress">${this.wordState.difficulty}</span>`)
+    div.insertAdjacentHTML(
+      'afterbegin',
+      `<i data-source="${this.wordState.audio}" class="material-icons" id="vocabulary-sound">volume_up</i>
+                                        <span class="vocabulary__word">${
+      this.wordState.word
+      }</span> 
+                                        <span class="new badge" data-badge-caption="progress">${
+      this.wordState.difficulty
+      }</span>`,
+    );
     return div;
   }
 
-  createBody () {
+  createBody() {
     const div = ElementGen('div', 'collapsible-body', this.cardElem);
-    div.insertAdjacentHtml('afterbegin', `<div class="row">
+    div.insertAdjacentHTML('afterbegin', `<div class="row">
                     <p class="col s6">${this.wordState.wordTranslate}</p>
                     <p class="col s6">${this.wordState.transcription}</p>
                   </div> 
-                  <img  src="${dataController.getMaterials(this.wordState.image).then((fullPath) => fullPath)}" alt="">
+                  <img  src="${this.wordState.image}" alt="">
                   <div class="sentences"> 
-                    <p>${this.wordState.textMeaning} <i class="material-icons" data-source="${dataController.getMaterials(this.wordState.audioMeaning).then((fullPath) => fullPath)}">volume_up</i></p>
+                    <p>${
+      this.wordState.textMeaning
+      } <i class="material-icons" data-source="${this.wordState.audioExample}" id="vocabulary-sound" onclick = "alert('Клик!')>volume_up</i></p>
                     <p>${this.wordState.textMeaningTranslate}</p>
-                    <p>${this.wordState.textExample} <i data-source="${dataController.getMaterials(this.wordState.audioExample).then((fullPath) => fullPath)}" class="material-icons">volume_up</i></p>
+                    <p>${this.wordState.textExample} <i data-source="${this.wordState.audioMeaning}" class="material-icons" id="vocabulary-sound">volume_up</i></p>
                     <p>${this.wordState.textExampleTranslate}</p>
                   </div>
                   <div class="divider"></div>
                   <div class="vocabulary__card-footer">
-                    <p>Последнее повторение было: <span class="last-itaretion-date">${this.wordState.date}</span></p>
-                    <p>Следующее повторение будет: <span class="last-itaretion-date">${this.wordState.date}</span></p>
+                    <p>Последнее повторение было: <span class="last-itaretion-date">${
+      this.wordState.date
+      }</span></p>
+                    <p>Следующее повторение будет: <span class="last-itaretion-date">${
+      this.wordState.date
+      }</span></p>
                   </div>`);
     return div;
+  }
+
+  renderWordsContainerColum(wordsInColum, colum) {
+    const ul = ElementGen('ul', `on-learn__column${colum} collapsible col m6 l6 s12`, this.cardElem);
+    wordsInColum.forEach(element => {
+      const word = element;
+      Promise.all([dataController.getMaterials(element.image).then((fullPath) => {
+        word.image = fullPath
+      }),
+      dataController.getMaterials(element.audio).then((fullPath) => { word.audio = fullPath }),
+      dataController.getMaterials(element.audioExample).then((fullPath) => { word.audioExample = fullPath }),
+      dataController.getMaterials(element.audioMeaning).then((fullPath) => { word.audioMeaning = fullPath })]).
+        then(() => {
+          this.wordState = word;
+          const li = ElementGen('li', 'vocabulary__word-container');
+          li.appendChild(this.createHeader());
+          li.appendChild(this.createBody());
+          ul.appendChild(li);
+        }
+        )
+    })
+    return ul;
+  }
+
+  renderVocabularyWords(words, category) {
+    const { paginatedResults } = words[0];
+    category.appendChild(
+      this.renderWordsContainerColum(paginatedResults.slice(0, Math.ceil(paginatedResults.length / 2)), 1),
+    );
+    category.appendChild(
+      this.renderWordsContainerColum(paginatedResults.slice(Math.ceil(paginatedResults.length / 2)), 2),
+    );
+
+    // eslint-disable-next-line no-undef
+    M.AutoInit()
+  }
+
+  playSound() {
+    const vocabularySound = document.querySelectorAll('#vocabulary-sound');
+    vocabularySound.forEach(element => {
+      element.addEventListener('click', (e) => {
+        console.log(e);
+      });
+    });
+  }
+
+  renderVocabulary() {
+    dataController.userWordsGetAll(['onlearn']).then(
+      (response) => {
+        this.renderVocabularyWords(response, vocabularyOnLearn)
+      },
+      (rejectReport) => rejectReport
+    );
+
+    dataController.userWordsGetAll(['hard']).then(
+      (response) => {
+        this.renderVocabularyWords(response, vocabularyDifficult)
+      },
+      (rejectReport) => rejectReport
+    );
+
+    dataController.userWordsGetAll(['deleted']).then(
+      (response) => {
+        this.renderVocabularyWords(response, vocabularyDeleted);
+      },
+      (rejectReport) => rejectReport
+    );
   }
 }
