fdce6906f544d5ac06e7e3e9ca26795b53c3c6e8
Iuliia Plekhanova <julec2006@yandex.ru>
2020-07-08 01:39:56 +0400

    feat: add speak it stats table

--- src/pages/index/js/components/statTable.js
+++ src/pages/index/js/components/statTable.js
@@ -67,6 +67,25 @@ const renderSprintStats = (fullStatObj) => {
   });
 }
 
+const renderSpeakItStats = (fullStatObj) => {
+  const tBody = document.querySelector('#stat-speakit tbody');
+  const stats = fullStatObj['speak-it'].longTime;
+  stats.forEach((roundStat) => {
+    const tr = ElementGen('tr', '', tBody);
+    const td1 = ElementGen('td', '', tr);
+    const td2 = ElementGen('td', '', tr);
+    const td3 = ElementGen('td', '', tr);
+    const td4 = ElementGen('td', '', tr);
+    const td5 = ElementGen('td', '', tr);
+
+    td1.insertAdjacentText('afterbegin', roundStat.round);
+    td2.insertAdjacentText('afterbegin', roundStat.date);
+    td3.insertAdjacentText('afterbegin', roundStat.result);
+    td4.insertAdjacentText('afterbegin', roundStat.knownWords);
+    td5.insertAdjacentText('afterbegin', roundStat.mistakeWords);
+  });
+}
+
 const renderMatchItStats = (fullStatObj) => {
   const tBody = document.querySelector('#stat-matchit tbody');
   const stats = fullStatObj['match-it'].longTime;
@@ -92,4 +111,5 @@ export const renderTable = (fullStatObj) => {
   renderSavannaStats(fullStatObj);
   renderMatchItStats(fullStatObj);
   renderSprintStats(fullStatObj);
+  renderSpeakItStats(fullStatObj);
 }
