c98224d194de0886740bcca64c290dbda6b262fa
ILokalin <51790923+ILokalin@users.noreply.github.com>
2020-07-07 21:24:32 +0500

    feat(preloaderView): update preloader view and correct set time for screen show

--- src/components/PreloaderView/PreloaderView.js
+++ src/components/PreloaderView/PreloaderView.js
@@ -1,14 +1,15 @@
-import { preloaderState } from 'Service/AppStore';
+import { preloaderState } from 'Service/AppState';
 import { DomGen } from 'Service/DomGen';
-import { messages } from './PreloaderConst';
+import { messages } from './PreloaderViewConst';
+import { preloaderViewScreen } from './PreloaderViewScreen';
 
-export class Preloader {
+export class PreloaderView {
   constructor() {
     this.body = document.body;
   }
 
   init() {
-    this.preloader = DomGen();
+    this.preloader = DomGen(preloaderViewScreen);
     this.countOfMessage = 0;
 
     preloaderState.watch((isShow) => {
@@ -21,23 +22,28 @@ export class Preloader {
   }
 
   startCycleAnimation() {
-    this.timeSet = 10000;
     setTimeout(() => {
       this.countOfMessage += 1;
-      if (this.countOfMessage < 7 && this.isHide) {
+      if (this.countOfMessage >= 7 && this.isHide) {
         this.preloader.block.remove();
+        this.countOfMessage = 0;
+      } else {
+        this.preloader.discribeLine.innerText = messages[this.countOfMessage];
+        this.startCycleAnimation();
       }
-      console.log(messages[this.countOfMessage]);
-      this.startCycleAnimation();
     }, this.timeSet);
   }
 
   showPreloader() {
+    this.timeSet = 5000;
+    this.isHide = false;
     this.body.append(this.preloader.block);
+    this.preloader.discribeLine.innerText = messages[this.countOfMessage];
     this.startCycleAnimation();
   }
 
   hidePreloader() {
+    this.timeSet = 500;
     this.isHide = true;
   }
 }
