37800422ba388fe33252a51bd4b0aac732270a30
ANDREI AHUREIKIN <ahureikinandrei@gmail.com>
2020-07-12 13:47:54 +0300

    fix: game time

--- src/pages/sprint/components/gameSprint/GameSprint.js
+++ src/pages/sprint/components/gameSprint/GameSprint.js
@@ -77,7 +77,7 @@ let wrongRight = [];
 
 export default class GameSprint {
   constructor(score) {
-    this.gameTime = 30;
+    this.gameTime = 60;
     this.currentGameTime = 0;
     this.stopGameTime = 0;
     this.stepTime = 1;
